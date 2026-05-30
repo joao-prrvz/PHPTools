f619daa7d95fba561f4f1147db1c35578a493441
Joao Victor Pereira Vaz <joao.prrvz@eduge.ch>
2026-05-30 10:37:38 +0200

    Important, added username, password, and options params to a DBContext contructor

--- src/ORM/DBContext.php
+++ src/ORM/DBContext.php
@@ -7,8 +7,13 @@ abstract class DBContext {
     private PDO $pdo;
     public PDO $db { get => $this->pdo; }
     
-    public function __construct(string $dsn) {
-        $this->pdo = new PDO($dsn);
+    public function __construct(
+        string $dsn, 
+        string|null $username = null,
+        string|null $password = null,
+        array|null $options = null
+    ) {
+        $this->pdo = new PDO($dsn, $username, $password, $options);
     }
 
     public function run(string $sql, array $params = []) {
